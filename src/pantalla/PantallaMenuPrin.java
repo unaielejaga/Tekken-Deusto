0384a664423b599f554ce31aaa757553d609fd39
Unai <unai.elejaga@opendeusto.es>
2018-09-25 13:52:19 +0200

    Fondo de Pantalla

--- src/pantalla/PantallaMenuPrin.java
+++ src/pantalla/PantallaMenuPrin.java
@@ -4,17 +4,24 @@ import javax.swing.JFrame;
 
 public class PantallaMenuPrin extends JFrame{
 	
+	private JPanelBackground fondo;
+	
 	public PantallaMenuPrin() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setSize(1920, 1080);
 		setTitle("Pantalla Menú Principal");
 		setResizable(false);
+		fondo = new JPanelBackground();
+		fondo.setBackground("fondo.jpg");
+		getContentPane().add(fondo);
 		
 		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PantallaMenuPrin p = new PantallaMenuPrin();
+		p.setVisible(true);
+				
 
 	}
 
