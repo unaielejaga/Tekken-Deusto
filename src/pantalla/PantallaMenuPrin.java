1f056f2c54a88ed681b014870622df3681812d53
Unai <unai.elejaga@opendeusto.es>
2018-09-25 13:43:43 +0200

    Pantalla Menu Principal

---
+++ src/pantalla/PantallaMenuPrin.java
@@ -0,0 +1,21 @@
+package pantalla;
+
+import javax.swing.JFrame;
+
+public class PantallaMenuPrin extends JFrame{
+	
+	public PantallaMenuPrin() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(1920, 1080);
+		setTitle("Pantalla Menú Principal");
+		setResizable(false);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
