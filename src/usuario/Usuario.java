aba68e9d6a5daeac0610f3d34f84cc4197ec3d6e
Unai <unai.elejaga@opendeusto.es>
2018-11-21 16:17:32 +0100

    Cambio en la clase Usuario

--- src/usuario/Usuario.java
+++ src/usuario/Usuario.java
@@ -3,8 +3,7 @@ package usuario;
 public class Usuario {
 	private String nick;
 	private String contrasenya;
-	private int combG;
-	private int combP;
+
 	
 	
 	public String getNick() {
@@ -22,38 +21,12 @@ public class Usuario {
 	public void setContrasenya(String contrasenya) {
 		this.contrasenya = contrasenya;
 	}
-	
-	public int getCombG() {
-		return combG;
-	}
-	
-	public void setCombG(int combG) {
-		this.combG = combG;
-	}
-	
-	public int getCombP() {
-		return combP;
-	}
-	
-	public void setCombP(int combP) {
-		this.combP = combP;
-	}
-	
-	
+		
 	// Constructor que pide nombre y contraseña
 	public Usuario(String nombre, String contrasenya) {
 		this.nick = nombre;
 		this.contrasenya = contrasenya;
 	}
-	
-	// Metodo en el que se le suma uno a la variable combG del Usuario si ha ganado el combate
-	public void GanarComb() {
-		this.combG = this.combG + 1;
-	}
-	
-	// Metodo en el que se le suma uno a la variable combP del Usuario si ha perdido el combate
-	public void PerderComb( ) {
-		this.combP = this.combP + 1;
-	}
+
 
 }
